7bfb48cc3ef94cb739581e51bce9cf2864afc8eb
alpayyyy <alpay.erkun3@hotmail.com>
2026-03-07 17:07:50 +0300

    Vue 3 ve Tailwind CSS arayüzü eklendi. Ana sayfa vitrini ve tekil yazı detayı tamamlandı.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// Vue uygulamasını oluştur ve #app id'li HTML etiketine yerleştir
+createApp(App).mount('#app');
